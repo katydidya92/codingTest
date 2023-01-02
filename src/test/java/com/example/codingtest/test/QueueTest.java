ac626b57109a9cf337b702fd0b5b8fe57bbda6ea
katydidya92 <sweets1327@gmail.com>
2023-01-03 06:45:45 +0900

    자료구조 - Queue 관련 로직 작성

---
+++ src/test/java/com/example/codingtest/test/QueueTest.java
@@ -0,0 +1,53 @@
+package com.example.codingtest.test;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueTest<T> {
+
+    @Test
+    void queueTest() {
+        Queue<Integer> queueList = new LinkedList<>();
+
+        queueList.add(1);
+        queueList.offer(2);
+        System.out.println(queueList);
+
+        queueList.poll();
+        System.out.println(queueList);
+
+        queueList.add(1);
+        System.out.println(queueList);
+
+        queueList.remove();
+        System.out.println(queueList);
+
+
+        System.out.println("=========================");
+
+        QueueTest<Integer> myQueue = new QueueTest<>();
+        myQueue.enQueue(1);
+        myQueue.enQueue(2);
+        myQueue.enQueue(3);
+        myQueue.enQueue(4);
+        myQueue.enQueue(5);
+        System.out.println(myQueue.deQueue());
+        System.out.println(myQueue.deQueue());
+    }
+
+    private ArrayList<T> queue = new ArrayList<>();
+
+    public void enQueue(T item) {
+        queue.add(item);
+    }
+
+    public T deQueue() {
+        if (queue.isEmpty()) {
+            return null;
+        }
+        return queue.remove(0);
+    }
+}
