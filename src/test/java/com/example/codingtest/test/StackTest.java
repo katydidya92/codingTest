a521f3383b77939d6e3fe4fbb1af3dead2b0b710
katydidya92 <sweets1327@gmail.com>
2023-01-03 06:54:20 +0900

    자료구조 - Stack 관련 로직 작성

---
+++ src/test/java/com/example/codingtest/test/StackTest.java
@@ -0,0 +1,47 @@
+package com.example.codingtest.test;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class StackTest<T> {
+
+    @Test
+    void stackTest() {
+        Stack<Integer> stackList = new Stack<>();
+        stackList.push(1);
+        stackList.push(2);
+        System.out.println(stackList);
+
+        stackList.pop();
+        System.out.println(stackList);
+
+        stackList.push(1);
+        System.out.println(stackList);
+
+        System.out.println("========================");
+
+        StackTest<Integer> myStack = new StackTest<>();
+        myStack.push(1);
+        myStack.push(2);
+        myStack.push(3);
+        myStack.push(4);
+
+        System.out.println(myStack.pop());
+        System.out.println(myStack.pop());
+    }
+
+    private ArrayList<T> arrayList = new ArrayList<>();
+
+    public void push(T item) {
+        arrayList.add(item);
+    }
+
+    public T pop() {
+        if (arrayList.isEmpty()) {
+            return null;
+        }
+        return arrayList.remove(arrayList.size() - 1);
+    }
+}
